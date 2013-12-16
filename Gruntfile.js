f6cb4e73dcb83049d357cc8529887a3aa583de9a
Tom Wayson <tom@tomwayson.com>
2013-12-16 07:57:17 -0800

    added templated widget example. resolves #2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,23 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-requirejs');
 
     grunt.initConfig({
-        requirejs: {
-            compile: {
-                options: {
-                    appDir: "js",
-                    baseUr: ".",
-                    dir: "dist",
-                    paths: {
-                        "esri" : "empty:",
-                        "dojo" : "empty:",
-                        "dojox" : "empty:",
-                        "dijit" : "empty:",
-                        "text" : "empty:"
-                    },
-                    optimize: "uglify2"
-                }
-            }
+      requirejs: {
+        compile: {
+          options: {
+            baseUrl: '.',
+            appDir: 'js/',
+            dir: 'dist',
+            paths: {
+              'esri': 'empty:',
+              'dojo': 'empty:',
+              'dojox': 'empty:',
+              'dijit': 'empty:'
+            },
+            inlineText: true,
+            optimize: 'uglify2'
+          }
         }
+      }
     });
 
     grunt.registerTask("build", ["requirejs"]);
