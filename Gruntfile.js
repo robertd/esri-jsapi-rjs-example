d4ab85eeced0294220883c7317b0aed5f1db2fdd
Tom Wayson <tom@tomwayson.com>
2013-12-16 17:42:47 -0800

    grunt tasks build deployable apps w/ all files. resolves #5

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function (grunt) {
       compile: {
         options: {
           baseUrl: '.',
-          appDir: 'js/',
+          appDir: 'src',
           dir: 'dist',
           paths: {
             'esri': 'empty:',
@@ -16,22 +16,21 @@ module.exports = function (grunt) {
             'dojox': 'empty:',
             'dijit': 'empty:'
           },
-          inlineText: true,
           optimize: 'uglify2'
         }
       },
       single: {
         options: {
-          baseUrl: 'js',
+          baseUrl: 'src/js',
           name: 'main',
-          out: 'dist/main-built.js',
+          out: 'dist/js/main-built.js',
           paths: {
             'esri': 'empty:',
             'dojo': 'empty:',
             'dojox': 'empty:',
             'dijit': 'empty:',
-            'text': '../deps/text/text',
-            'domReady': '../deps/domReady/domReady'
+            'text': '../../deps/text/text',
+            'domReady': '../../deps/domReady/domReady'
           },
           exclude: ['esri', 'dojo', 'dojox', 'dijit', 'text', 'domReady'],
           inlineText: true,
@@ -41,17 +40,33 @@ module.exports = function (grunt) {
     },
 
     replace: {
-      example: {
-        src: ['dist/main-built.js'],
-        dest: 'dist/main-built.js',
+      "main-built": {
+        src: ['dist/js/main-built.js'],
+        dest: 'dist/js/main-built.js',
         replacements: [{
           from: 'text!',
           to: ''
         }]
+      },
+      index: {
+        src: ['src/index.html'],
+        dest: 'dist/index.html',
+        replacements: [{
+          from: 'js/main.js',
+          to: 'js/main-built.js'
+        },
+        {
+          from: '{ name: "app",',
+          to: '// { name: "app",'
+        }]
       }
+    },
+
+    clean: {
+      single: ['dist']
     }
   });
 
-  grunt.registerTask("build", ["requirejs:compile"]);
-  grunt.registerTask("single", ["requirejs:single", "replace"]);
+  grunt.registerTask("compile", ["requirejs:compile"]);
+  grunt.registerTask("single", ["clean", "requirejs:single", "replace"]);
 };
