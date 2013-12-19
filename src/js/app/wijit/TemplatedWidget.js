d7e0c4e0f1e879e0da8b7ceb4857babaff08754a
robertd <robert.djurasaj@gmail.com>
2013-12-19 10:23:51 -0700

    Add i18n example. Resolves #12

--- src/js/app/wijit/TemplatedWidget.js
+++ src/js/app/wijit/TemplatedWidget.js
@@ -2,12 +2,14 @@ define([
     "dojo/_base/declare",
     "dijit/_WidgetBase",
     "dijit/_TemplatedMixin",
-    "text!app/wijit/templates/TemplatedWidget.html"
-], function(declare, _WidgetBase, _TemplatedMixin, template) {
+    "text!app/wijit/templates/TemplatedWidget.html",
+    "i18n!app/wijit/nls/colors"
+], function(declare, _WidgetBase, _TemplatedMixin, template, colors) {
 
     return declare([_WidgetBase, _TemplatedMixin], {
         baseClass: "templatedWidget",
-        templateString: template
+        templateString: template,
+        color: colors.blue
         //  your custom code goes here
     });
 
